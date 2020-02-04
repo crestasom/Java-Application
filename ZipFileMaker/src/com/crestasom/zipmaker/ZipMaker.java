f579844a6c9b20ea241b3313dbbd3a0a8c1e539c
crestasom <crestasom@gmail.com>
2020-02-04 13:45:07 +0545

    UI changed

--- ZipFileMaker/src/com/crestasom/zipmaker/ZipMaker.java
+++ ZipFileMaker/src/com/crestasom/zipmaker/ZipMaker.java
@@ -2,7 +2,6 @@ package com.crestasom.zipmaker;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -12,48 +11,28 @@ import net.lingala.zip4j.model.ZipParameters;
 import net.lingala.zip4j.util.Zip4jConstants;
 
 public class ZipMaker {
-	/*
-	 * public static void main(String[] args) { ZipMaker pzip = new ZipMaker(); try
-	 * { pzip.compressWithPassword("c:\\temp"); } catch (ZipException e) { // TODO
-	 * Auto-generated catch block e.printStackTrace(); }
-	 * 
-	 * 
-	 * try { pzip.unCompressPasswordProtectedFiles("G:\\files.zip"); } catch
-	 * (ZipException e) { // TODO Auto-generated catch block e.printStackTrace(); }
-	 * 
-	 * 
-	 * }
-	 */
-
 	/**
 	 * Method for creating password protected zip file
 	 * 
 	 * @param sourcePath
+	 * @param destDir
 	 * @throws ZipException
 	 * @throws IOException
 	 */
-	public static void compressWithPassword(String sourcePath) throws ZipException, IOException {
-		String passFile = sourcePath + "/password.csv";
-		File password = new File(passFile);
-		System.out.println("sourcePath:" + sourcePath);
+	public static void compressWithPassword(String sourcePath, String destPath) throws ZipException, IOException {
+		String passFile = "/password.csv";
+		File password = new File(destPath + "//" + passFile);
 		File root = new File(sourcePath);
-		String[] dirs = root.list(new FilenameFilter() {
-
-			@Override
-			public boolean accept(File dir, String name) {
-				// TODO Auto-generated method stub
-				System.out.println(name);
-				return new File(dir, name).isDirectory();
-			}
+		// find all the directories inside the source folder
+		String[] dirs = root.list((dir, name) -> {
+			return new File(dir, name).isDirectory();
 		});
 		FileWriter writer = new FileWriter(password);
 		for (String dir : dirs) {
 			writer.write(dir + "," + dir + "\n");
 			String srcPath = sourcePath + "\\" + dir;
-			System.out.println("srcPath:" + srcPath);
-			String destPath = srcPath + ".zip";
-			System.out.println("Destination " + destPath);
-			ZipFile zipFile = new ZipFile(destPath);
+			String destDir = destPath + "\\" + dir + ".zip";
+			ZipFile zipFile = new ZipFile(destDir);
 			// Setting parameters
 			ZipParameters zipParameters = new ZipParameters();
 			zipParameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
@@ -76,25 +55,4 @@ public class ZipMaker {
 		writer.close();
 	}
 
-	/**
-	 * Method for unzipping password protected file
-	 * 
-	 * @param sourcePath
-	 * @throws ZipException
-	 */
-	private void unCompressPasswordProtectedFiles(String sourcePath) throws ZipException {
-		String destPath = getFileName(sourcePath);
-		System.out.println("Destination " + destPath);
-		ZipFile zipFile = new ZipFile(sourcePath);
-		// If it is encrypted then provide password
-		if (zipFile.isEncrypted()) {
-			zipFile.setPassword("pass@123");
-		}
-		zipFile.extractAll(destPath);
-	}
-
-	private String getFileName(String filePath) {
-		// Get the folder name from the zipped file by removing .zip extension
-		return filePath.substring(0, filePath.lastIndexOf("."));
-	}
 }
--- ZipFileMaker/src/com/crestasom/zipmaker/ZipUI.java
+++ ZipFileMaker/src/com/crestasom/zipmaker/ZipUI.java
@@ -1,59 +1,119 @@
 package com.crestasom.zipmaker;
 
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-
 import net.lingala.zip4j.exception.ZipException;
 
 public class ZipUI extends JFrame implements ActionListener {
-	JPanel mainPanel;
-	JButton folderCompress;
+
+	private static final long serialVersionUID = 1L;
+	JPanel mainPanel, sourcePanel, destPannel, actionPanel;
+	JButton srcFolderBtn, destFolderBtn, compressBtn, resetBtn;
+	JLabel srcFolderLbl, destFolderLbl;
 	JFileChooser fc;
-	String rootDir;
-	File choosenFolder;
+	String rootDir, srcDir, destDir;
+	File srcFolder, destFolder;
 
 	public static void main(String[] args) {
 		new ZipUI();
 	}
+
 	public ZipUI() {
 		super("Folder Zipper");
 		mainPanel = new JPanel();
 		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-		folderCompress = new JButton("Compress from a folder");
-		folderCompress.addActionListener(this);
-		mainPanel.add(folderCompress);
+
+		sourcePanel = new JPanel();
+		sourcePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+		srcFolderBtn = new JButton("Choose Source Folder");
+		srcFolderBtn.addActionListener(this);
+		sourcePanel.add(srcFolderBtn);
+		srcFolderLbl = new JLabel("\tNo Folder Selected");
+		sourcePanel.add(srcFolderLbl);
+
+		destPannel = new JPanel();
+		destPannel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+		destFolderBtn = new JButton("Choose Destination Folder");
+		destFolderBtn.addActionListener(this);
+		destPannel.add(destFolderBtn);
+		destFolderLbl = new JLabel("No Folder Selected");
+		destPannel.add(destFolderLbl);
+
+		actionPanel = new JPanel();
+		actionPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+		compressBtn = new JButton("Compress");
+		compressBtn.addActionListener(this);
+		actionPanel.add(compressBtn);
+		resetBtn = new JButton("Reset");
+		resetBtn.addActionListener(this);
+		actionPanel.add(resetBtn);
+
+		mainPanel.add(sourcePanel);
+		mainPanel.add(Box.createVerticalGlue());
+		mainPanel.add(destPannel);
+		mainPanel.add(Box.createVerticalGlue());
+		mainPanel.add(actionPanel);
 		add(mainPanel);
-		pack();
+		setResizable(false);
+		setSize(500, 250);
 		setVisible(true);
 
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		if (e.getSource().equals(folderCompress)) {
+		if (e.getSource().equals(srcFolderBtn)) {
 			JFileChooser fc = new JFileChooser();
 			fc.setCurrentDirectory(new java.io.File(".")); // start at application current directory
 			fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 			int returnVal = fc.showSaveDialog(this);
 			if (returnVal == JFileChooser.APPROVE_OPTION) {
-				choosenFolder = fc.getSelectedFile();
-				rootDir = choosenFolder.getAbsolutePath();
+				srcFolder = fc.getSelectedFile();
+				srcDir = srcFolder.getAbsolutePath();
+				srcFolderLbl.setText(srcDir);
+			}
+		} else if (e.getSource().equals(destFolderBtn)) {
+			JFileChooser fc = new JFileChooser();
+			fc.setCurrentDirectory(new java.io.File(".")); // start at application current directory
+			fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			int returnVal = fc.showSaveDialog(this);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				destFolder = fc.getSelectedFile();
+				destDir = destFolder.getAbsolutePath();
+				destFolderLbl.setText(destDir);
+			}
+		} else if (e.getSource().equals(resetBtn)) {
+			srcDir = null;
+			destDir = null;
+			srcFolderLbl.setText("No Folder Selected");
+			destFolderLbl.setText("No Folder Selected");
+		} else if (e.getSource().equals(compressBtn)) {
+			if (srcDir != null && destDir != null) {
 				try {
-					ZipMaker.compressWithPassword(rootDir);
+					ZipMaker.compressWithPassword(srcDir, destDir);
+					JOptionPane.showMessageDialog(this,
+							"All folders inside " + rootDir + " were successfully zipped with password");
 				} catch (ZipException | IOException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-
+			} else {
+				JOptionPane.showMessageDialog(this, "Source or destination directory not selected", "Error",
+						JOptionPane.ERROR_MESSAGE);
 			}
 		}
 	}
